d2362711779cbb11e12cf3199cd47a66f60db897
CinthiaRod <Cinthiarb28@gmail.com>
2024-12-07 13:12:58 -0800

    Agregando descripción

---
+++ Ejercicio9.js
@@ -0,0 +1,4 @@
+//Ejercicio 9: Contar hasta un número 
+//Crea una función declarada llamada contarHasta que reciba un número
+// y use un bucle para imprimir todos los números desde 1 hasta ese número.
+
