545636c0ae8b79f6427d7cb15dc23f98088c936b
CinthiaRod <Cinthiarb28@gmail.com>
2024-12-07 13:00:04 -0800

    Agregando descripción

---
+++ Ejercicio4.js
@@ -0,0 +1,4 @@
+//Ejercicio 4: Número par o impar 
+//Crea una función declarada llamada esPar que reciba un número y 
+//devuelva "Es par" si el número es par o "Es impar" si el número es impar. 
+//Usa una variable local para guardar el resultado.
