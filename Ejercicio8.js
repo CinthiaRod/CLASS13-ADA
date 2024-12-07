966219cfc75a2ecfd39df5bb31a3485e0b2972a9
CinthiaRod <Cinthiarb28@gmail.com>
2024-12-07 13:11:30 -0800

    Agregando descripción

---
+++ Ejercicio8.js
@@ -0,0 +1,4 @@
+//Ejercicio 8: Convertir grados Celsius a Fahrenheit (Ejercicio entrevista) 
+//Crea una función expresada llamada convertirCelsiusAFahrenheit 
+//que reciba una temperatura en grados Celsius y devuelva la temperatura en Fahrenheit.
+
